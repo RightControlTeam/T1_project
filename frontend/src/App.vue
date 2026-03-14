857b7fcbf9f6d63a4d514d6109ca4cf7b7e696e7
DariaEFAL <EF.Daria.AL@yandex.ru>
2026-03-14 11:51:07 +0300

    feat: add login page

---
+++ frontend/src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+// Показывать шапку только НЕ на странице входа
+const showHeader = computed(() => route.path !== '/login')
+</script>
+
+<template>
+    <div>
+        <!-- Шапка только если showHeader = true -->
+        <header v-if="showHeader">
+            <nav>
+                <RouterLink to="/">Главная</RouterLink>
+                <button @click="logout">Выйти</button>
+            </nav>
+        </header>
+        
+        <!-- Здесь показываются страницы -->
+        <main>
+            <RouterView />
+        </main>
+    </div>
+</template>
+
+<script>
+// Отдельный скрипт для метода logout
+export default {
+    methods: {
+        logout() {
+            localStorage.removeItem('auth')
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    background: #42b883;
+    padding: 1rem;
+    color: rgb(104, 104, 104);
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+a {
+    color: rgb(0, 0, 0);
+    text-decoration: none;
+}
+
+button {
+    padding: 5px 10px;
+    background: rgb(0, 0, 0);
+    color: #42b883;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+</style>
---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,71 @@
+<script setup>
+    import LoginForm from '@/components/LoginForm.vue'
+    import { useRouter, RouterLink } from 'vue-router'
+
+    const router = useRouter()
+    
+    function onLogin() {
+        localStorage.setItem('auth', 'true')
+
+        router.push('/')
+    }
+</script>
+
+<template>
+    <div class="login-page">
+        <div class="login-card">
+            <h1>Вход</h1>
+            <LoginForm @success="onLogin"/>
+            <p class="to-register">Ещё нет аккаунта? <RouterLink to="">Зарегистрироваться</RouterLink></p> <!--Надо вставить путь до регистрации-->
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+    color: white;
+    font-size: 36px;
+}
+
+.login-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100vw;
+    min-height: 100vh;
+
+    background: rgb(167, 201, 255); /*можно попробывать разные цвета*/
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.login-card {
+    background: rgba(0, 98, 255, 0.35);
+    backdrop-filter: blur(20px);            /* размытие фона */
+    -webkit-backdrop-filter: blur(20px);    /* для Safari */
+    box-shadow: 0 0 10px rgba(0, 98, 255, 0.35);
+
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 400px;
+    padding: 16px;
+    margin: 16px; /*от края экрана должны быть отступы до формы*/
+    border-radius: 16px;
+}
+
+.to-register {
+    color: white;
+    font-size: 14px;
+}
+
+a {
+    color: #5D20ED;
+    text-decoration: none;  /* убирает подчеркивание */
+}
+</style>
