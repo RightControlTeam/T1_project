24e6bc1b38c5cb57ff468514b3b47d1c3da04a5f
Vika-program <borisovav308@gmail.com>
2026-04-20 22:07:27 +0300

    add bookings

--- frontend/src/views/BookingsPage.vue
+++ frontend/src/views/BookingsPage.vue
@@ -1,9 +1,43 @@
 <script setup>
+import api from '@/api/index'
+import { ref } from 'vue'
+
+const bookings = ref([])
+const currentUserId = ref(null)
+
+async function get_user_id() {
+  try {
+    const response = await api.get('/user/profile')
+    currentUserId.value = response.data.id
+    console.log('Текущий пользователь:', currentUserId.value)
+    return currentUserId.value
+  } catch (error) {
+    console.error('Ошибка получения пользователя:', error)
+    return null
+  }
+}
+async function get_bookings() {
+  try {
+    const user_id = await get_user_id()
+    const response = await api.get('/booking/', {
+            params: { user_id: user_id }
+        })
+    bookings.value = response.data
+    console.log(bookings.value)
+  } catch (e) {
+    console.log(e)
+  }
+}
+get_user_id()
+get_bookings()
 </script>
 
 <template>
   <div class="booking-page">
     <h1>BookingsPage</h1>
+    <div v-for="(booking, index) in bookings" :key="index">
+      <p v-if="booking.is_cancelled == false">Booking {{ booking}}</p>
+    </div>
   </div>
 </template>
 
