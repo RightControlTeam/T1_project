71164f63ca926fe110587133ea3de57345892fbe
Opik1704 <157130462+Opik1704@users.noreply.github.com>
2026-06-03 08:01:58 +0300

    Update schemas.py

--- backend/resource/schemas.py
+++ backend/resource/schemas.py
@@ -19,6 +19,11 @@ class ResourceScheduleCreate(BaseModel):
     day_of_week: int
     start_time: time
     end_time: time
+    
+class ResourceScheduleUpdate(BaseModel):
+    day_of_week: Optional[int] = None
+    start_time: Optional[time] = None
+    end_time: Optional[time] = None
 
 class ResourceScheduleOut(BaseModel):
     id: int
